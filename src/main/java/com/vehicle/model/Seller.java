3c8ea9e1696e235649beb6b9351ba0e41300200d
Dielson <d2021004346@unifei.edu.br>
2024-05-24 10:59:38 +0200

    [fix] Add manytomany in vehicle

--- src/main/java/com/vehicle/model/Seller.java
+++ src/main/java/com/vehicle/model/Seller.java
@@ -10,59 +10,58 @@ import jakarta.persistence.ManyToMany;
 
 @Entity
 public class Seller {
-	
-	@Id
-	private String credential;
-	private String nom;
-	
-	@ManyToMany
-	@JoinTable(name="projet_utilisateurs",joinColumns = @JoinColumn(name = "credential"),inverseJoinColumns = @JoinColumn(name = "id"))
-	private List<Vehicle> projets;
-	
-	public Seller() {
-		super();
-	}
-	
-	
-	public Seller(String credential, String nom) {
-		super();
-		this.credential = credential;
-		this.nom = nom;
-	}
-	
-	
+
+    @Id
+    private String credential;
+    private String nom;
+
+    @ManyToMany
+    @JoinTable(name = "seller_vehicle", joinColumns = @JoinColumn(name = "credential"), inverseJoinColumns = @JoinColumn(name = "id"))
+    private List<Vehicle> projets;
+
+    public Seller() {
+        super();
+    }
 
 
-	public List<Vehicle> getProjets() {
-		return projets;
-	}
+    public Seller(String credential, String nom) {
+        super();
+        this.credential = credential;
+        this.nom = nom;
+    }
 
 
-	public void setProjets(List<Vehicle> projets) {
-		this.projets = projets;
-	}
+    public List<Vehicle> getProjets() {
+        return projets;
+    }
 
 
-	public String getCredential() {
-		return credential;
-	}
-	public void setCredential(String matricule) {
-		this.credential = matricule;
-	}
-	public String getNom() {
-		return nom;
-	}
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
+    public void setProjets(List<Vehicle> projets) {
+        this.projets = projets;
+    }
 
 
-	@Override
-	public String toString() {
-		return "Utilisateur [matricule=" + credential + ", nom=" + nom + "]";
-	}
-	
-	
-	
-	
+    public String getCredential() {
+        return credential;
+    }
+
+    public void setCredential(String matricule) {
+        this.credential = matricule;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Utilisateur [matricule=" + credential + ", nom=" + nom + "]";
+    }
+
+
 }
--- src/main/java/com/vehicle/model/Vehicle.java
+++ src/main/java/com/vehicle/model/Vehicle.java
@@ -16,83 +16,80 @@ import jakarta.persistence.OneToMany;
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
 public class Vehicle {
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private int id;
-	private String brand;
-	private String model;
-	private String year;
-	private String color;
-	
-	/*
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String brand;
+    private String model;
+    private String year;
+    private String color;
 
-	@OneToMany(mappedBy = "projet")
-	private List<Tache> taches;
-	
-	@ManyToMany
-	@JoinTable(name="projet_utilisateurs",joinColumns = @JoinColumn(name = "id"),inverseJoinColumns = @JoinColumn(name = "matricule"))
-	private List<Utilisateur> utilisateurs;
-	*/
-	
-	
-	
-	public Vehicle() {
-		super();
-	}
-	
-	
-	public Vehicle(int id, String brand, String model, String year, String color) {
-		super();
-		this.id = id;
-		this.brand = brand;
-		this.model = model;
-		this.year = year;
-		this.color = color;
-	}
-	
-	
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getBrand() {
-		return brand;
-	}
-	public void setBrand(String brand) {
-		this.brand = brand;
-	}
-	public String getModel() {
-		return model;
-	}
-	public void setModel(String model) {
-		this.model = model;
-	}
-	public String getYear() {
-		return year;
-	}
-	public void setYear(String year) {
-		this.year = year;
-	}
-	public String getColor() {
-		return color;
-	}
-	public void setColor(String color) {
-		this.color = color;
-	}
-	@Override
-	public String toString() {
-		return "Vehicle [id=" + id + ", brand=" + brand + ", model=" + model + ", year=" + year + ", color=" + color
-				+ "]";
-	}
-	
-	
+    @OneToMany(mappedBy = "projet")
+    private List<Tache> taches;
 
-	
-	
-	
-	
-	
+    @ManyToMany
+    @JoinTable(name = "seller_vehicle", joinColumns = @JoinColumn(name = "id"), inverseJoinColumns = @JoinColumn(name = "credential"))
+    private List<Seller> sellers;
+
+    public Vehicle() {
+        super();
+    }
+
+
+    public Vehicle(int id, String brand, String model, String year, String color) {
+        super();
+        this.id = id;
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle [id=" + id + ", brand=" + brand + ", model=" + model + ", year=" + year + ", color=" + color + "]";
+    }
+
+
 }
